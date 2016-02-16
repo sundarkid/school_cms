0fbc5603f58e21d3a45cd845614c40554a4330aa
Sundareswaran <sundareswaran25@gmail.com>
2016-02-16 21:46:00 +0530

    First commit after including the basic required files.

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>CMS Login | My School Notification</title>
+    <meta name="description" content="School Name">
+
+    <!-- Latest compiled and minified CSS (03-02-2016) -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+
+    <!-- Optional theme (03-02-2016) -->
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+
+    <!-- My Custom Style Sheet -->
+    <link rel="stylesheet" href="css/my-styles.css">
+    <!-- Malspup Image slider Slides -->
+    <link rel="stylesheet" href="css/bootstrap-carousel.css">
+    <!-- For improved cross-browser rendering, we use Normalize.css, a project by Nicolas Gallagher and Jonathan Neal. (03-02-2016) -->
+    <link rel="stylesheet" href="css/normalize.css">
+
+
+    <!-- JQuery file 2.2.0 -->
+    <script src="js/jquery-2.2.0.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<h3>Sundar</h3>
+
+</body>
+</html>
